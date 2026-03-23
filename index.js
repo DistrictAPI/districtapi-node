edb0cdc260609f2e1253fcca6a2d22cc8b616e40
DistrictAPI <districtapi@k12tech.com>
2026-03-23 12:51:34 -0700

    feat: initial placeholder package

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * districtapi - Official Node.js SDK for districtapi.dev
+ * 
+ * This is a placeholder package. The full SDK is in development.
+ * Visit https://districtapi.dev for updates.
+ */
+
+const VERSION = '0.1.0';
+
+function DistrictAPI() {
+  throw new Error(
+    'districtapi SDK is not yet available. ' +
+    'Visit https://districtapi.dev for updates.'
+  );
+}
+
+module.exports = { VERSION, DistrictAPI };
